9bcb4b0a012127a789df5ea50562b433598345a9
Zach Gongaware <zgongaware@gmail.com>
2019-06-01 10:16:07 -0700

    dashboard expansion

---
+++ baby_names/README.md
@@ -0,0 +1,8 @@
+# Baby Names Web Dashboard
+This repository is used to generate a web page featuring data dashboards on baby name trends in the United States from 1880 - 2018.
+
+[View the dashboard here.]("#")
+
+### Data Source
+Data is provided by the [Social Security Administration](https://www.ssa.gov/OACT/babynames/limits.html).
+
--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -4,6 +4,10 @@ import plotly.graph_objs as go
 
 
 def return_figures():
+    """
+    Retrieve data and generate figures for web page
+    :return:
+    """
 
     # Declare directory
     directory = "./data"
@@ -17,11 +21,74 @@ def return_figures():
     # Add charts
     figures.append(generate_time_series(names, "M", 5))
     figures.append(generate_time_series(names, "F", 5))
+    figures.append(generate_streaks_plot(names))
 
     return figures
 
 
+def generate_streaks_plot(dataframe):
+    """
+    Calculate streaks at rank 1 and generate bar plot
+    :param dataframe:
+    :return:
+    """
+
+    # Calculate streaks for each gender
+    males = get_streaks(dataframe, "M")
+    females = get_streaks(dataframe, "F")
+
+    # Combine dataframes
+    streaks = pd.concat([males, females])
+
+    # Add color by gender
+    streaks["color"] = streaks["gender"].apply(lambda x: "#347DC1" if x == "M" else "#E6A6C7")
+
+    # Sort by count
+    streaks.sort_values(by='count', inplace=True)
+
+    # Generate graph
+    graph = list()
+    graph.append(
+        go.Bar(
+            y=streaks.index.tolist(),
+            x=streaks["count"].tolist(),
+            marker={
+                "color": streaks["color"].tolist(),
+                "line": {
+                    "color": streaks["color"].tolist(),
+                    "width": 1.5
+                }
+            },
+            orientation="h",
+            opacity="0.7"
+        )
+    )
+
+    layout = {
+        "title": "Longest Streaks at Rank 1",
+        "xaxis": {
+            "title": "Years",
+            "autotick": True
+        },
+        "yaxis": {
+            "title": "Name"
+        },
+        "autosize": False,
+        "width": 500,
+        "height": 900,
+    }
+
+    return {"data": graph, "layout": layout}
+
+
 def generate_time_series(dataframe, gender, n):
+    """
+    Generate time series of top 5 baby names by gender
+    :param dataframe:
+    :param gender:
+    :param n:
+    :return:
+    """
 
     # Isolate to gender and sort by year/count
     df = dataframe[dataframe["gender"] == gender]
@@ -33,7 +100,7 @@ def generate_time_series(dataframe, gender, n):
     name_list = top_n["name"].unique().tolist()
 
     # Generate graph points by name
-    graph = []
+    graph = list()
     for name in name_list:
         x_val = top_n[top_n["name"] == name]["year"].tolist()
         y_val = top_n[top_n["name"] == name]["rank"].tolist()
@@ -64,10 +131,70 @@ def generate_time_series(dataframe, gender, n):
     return {"data": graph, "layout": layout}
 
 
+def get_streaks(dataframe, gender):
+    """
+    Generate a dataframe of longest streaks at rank 1 for a gender
+    :param dataframe:
+    :param gender:
+    :return:
+    """
+
+    # Isolate to gender
+    df = dataframe[dataframe["gender"] == gender]
+
+    # Get top 1 entries
+    top = get_yearly_top_n(df, 1)
+
+    # Calculate streaks
+    streaks = calculate_streaks(top)
+
+    # Add gender column
+    streaks["gender"] = gender
+
+    return streaks
+
+
+def calculate_streaks(dataframe):
+    """
+    Calculate streaks at rank 1 for a dataframe
+    :param dataframe:
+    :return:
+    """
+
+    # Isolate unique names
+    name_list = dataframe["name"].unique().tolist()
+
+    # Initiate empty dictionary
+    longest_streaks = {}
+
+    # Iterate over name list
+    for name in name_list:
+
+        streaks = []
+        year = 0
+        current_streak = 0
+
+        # Find streaks by year
+        for i, v in dataframe[dataframe["name"] == name].iterrows():
+            if int(v["year"]) == year + 1:
+                current_streak += 1
+            else:
+                streaks.append(current_streak)
+                current_streak = 1
+
+            year = int(v["year"])
+
+        streaks.append(current_streak)
+
+        longest_streaks[name] = max(streaks)
+
+    return pd.DataFrame.from_dict(longest_streaks, orient='index', columns=['count'])
+
+
 def get_yearly_top_n(dataframe, n):
     """
     Get top n records for each year within a dataframe
-    :param dataframe:
+    :param dataframe:a
     :param n:
     :return:
     """
